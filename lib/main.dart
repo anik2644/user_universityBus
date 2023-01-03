976a6c7f3b8c36c77f83bfe68a38f9d45d2d44ea
anik11556 <abdullahal-2019317827@cs.du.ac.bd>
2023-01-03 08:07:12 +0600

    share location showed

--- lib/main.dart
+++ lib/main.dart
@@ -105,7 +105,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 setState(() {
                   llong= position.longitude.toDouble();
                   llat =position.latitude.toDouble();
-
+ 
                 });
 
           }
