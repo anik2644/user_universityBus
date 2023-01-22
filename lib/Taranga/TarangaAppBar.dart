f65113378cacd9979eb1d225d2d8cbd357404288
anik11556 <abdullahal-2019317827@cs.du.ac.bd>
2023-01-22 13:04:28 +0600

    done

--- lib/Taranga/TarangaAppBar.dart
+++ lib/Taranga/TarangaAppBar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../SecondaryHomePage/SecondaryBody.dart';
+import 'TarangaHomePage.dart';
 
 class TarangaAppbar extends StatefulWidget {
   const TarangaAppbar({Key? key}) : super(key: key);
@@ -14,10 +15,11 @@ class _TarangaAppbarState extends State<TarangaAppbar> {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
+    return  AppBar(
       centerTitle: true,
       title: Text(
-        Bus.busList[Bus.selectedBus].name,
+        TarangaHomePage
+            .appbar_text,
         style: const TextStyle(
             fontSize: 20, color: Colors.white, fontWeight: FontWeight.w600),
       ),
