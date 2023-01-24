51b404370eb1173f0bc7f612b53238097678ae52
anik11556 <abdullahal-2019317827@cs.du.ac.bd>
2023-01-25 03:37:23 +0600

    done

--- lib/ParticularDetails/TarangaBusBody/UPtripsDowntrips/LocationShareButton.dart
+++ lib/ParticularDetails/TarangaBusBody/UPtripsDowntrips/LocationShareButton.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:location/location.dart' as loc;
 import 'package:userapp/StaticPart/FirabaseStaticVariables.dart';
+import 'package:userapp/StaticPart/Firebase/FirebaseLocationWrite.dart';
+import 'package:userapp/StaticPart/Firebase/FirebaseUpdate.dart';
 import 'package:userapp/StaticPart/ModelStatic.dart';
 
 import '../../../StaticPart/BusStaticVariables.dart';
@@ -23,6 +25,37 @@ class _LocationShareButtonState extends State<LocationShareButton> {
 
   String latt = "0.00";
   String lonn = "0.00";
+  int timeRestartFlag = 1;
+
+
+
+  void _updateNotice()
+  {
+
+    int flag = 0;
+    for (int i = 0;
+    i < widget._noticeController.text.length;
+    i++) {
+      if (widget._noticeController.text.codeUnitAt(i) > 64 &&
+          widget._noticeController.text.codeUnitAt(i) < 91 ||
+          widget._noticeController.text.codeUnitAt(i) > 96 &&
+              widget._noticeController.text.codeUnitAt(i) <
+                  123) {
+        flag = 1;
+        break;
+      }
+    }
+    if (flag == 1) {
+      BusStaticVariables.Notice = widget._noticeController.text;
+    }
+
+  }
+  void _updateLocshareFlag()
+  {
+    BusStaticVariables.locShare[ ModelStatic.location_share_schedule_index] = "0";
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -30,131 +63,96 @@ class _LocationShareButtonState extends State<LocationShareButton> {
         onPressed: () async {
 
           ModelStatic.location_share_schedule_index = widget.index;
+          ModelStatic.start_time = new DateTime.now();
+          
+          //for enable location on
           bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-          if (!serviceEnabled) {
-            await Geolocator.getCurrentPosition();
-          }
+          if (!serviceEnabled) {await Geolocator.getCurrentPosition();}
 
-          int flag = 0;
-          for (int i = 0;
-          i < widget._noticeController.text.length;
-          i++) {
-            if (widget._noticeController.text.codeUnitAt(i) > 64 &&
-                widget._noticeController.text.codeUnitAt(i) < 91 ||
-                widget._noticeController.text.codeUnitAt(i) > 96 &&
-                    widget._noticeController.text.codeUnitAt(i) <
-                        123) {
-              flag = 1;
-              break;
-            }
-          }
-          if (flag == 1) {
-            BusStaticVariables.Notice = widget._noticeController.text;
-          }
+          _updateNotice();
+          _updateLocshareFlag();
+          FirebaseUpdate.updateLocshareAndNotice();
 
-
-
-          BusStaticVariables.locShare[widget.index] = "0";
-          ModelStatic.location_share_schedule_index = widget.index;
-
-          print(FirebaseStaticVAriables.selected_schedule_id);
-
-          await FirebaseFirestore.instance
-              .collection('schedule')
-              .doc(FirebaseStaticVAriables.selected_schedule_id)
-              .update({
-            "locShare": BusStaticVariables.locShare,
-            'notice': BusStaticVariables.Notice
-          });
-
-          //gpsshereflag
 
           if (ModelStatic.gps_share_flag == 0) {
+
+
             loc.Location location = new loc.Location();
             location.enableBackgroundMode(enable: true);
-            print(location
-                .getLocation()
-                .then((value) => print(value.longitude)));
-            await location.changeSettings(
-                accuracy: loc.LocationAccuracy.high,
-                distanceFilter: 1);
+            await location.changeSettings(accuracy: loc.LocationAccuracy.high, distanceFilter: 1);
 
-            ModelStatic.locationSubscription =
-                location.onLocationChanged.listen(
+            ModelStatic.locationSubscription = location.onLocationChanged.listen(
                         (loc.LocationData currentLocation) async {
 
+                    timeRestartFlag = _timeTrack();
+                    _timeFlagAction(timeRestartFlag);
 
-                      if (ModelStatic.gps_share_flag == 1) {
-                        // print("with come");
-                        int time_flag = 1;
+                    FirebaseLocationWrite.locationWrite( currentLocation.latitude!, currentLocation.longitude!);
+                    _updateAppBar(currentLocation.latitude!.toString(), currentLocation.longitude!.toString());
 
-                        DateTime current_time = new DateTime.now();
-
-                        if (current_time.year >
-                            ModelStatic.start_time.year) {
-                          time_flag = 0;
-                        } else if (current_time.month >
-                            ModelStatic.start_time.month) {
-                          time_flag = 0;
-                        } else if (current_time.day >
-                            ModelStatic.start_time.day) {
-                          time_flag = 0;
-                        } else if (current_time.hour >
-                            ModelStatic.start_time.hour + 4) {
-                          time_flag = 0;
-                        }
-
-
-
-                        if (time_flag == 0) {
-                          loc.Location.instance
-                              .enableBackgroundMode(enable: false);
-                          ModelStatic.locationSubscription
-                              .cancel();
-
-                          ModelStatic.gps_share_flag = 0;
-                          print("app will restart");
-                        }
-
-                      }
-
-
-                      print(currentLocation.latitude!);
-                      print(currentLocation.longitude!);
-
-
-                      await FirebaseFirestore.instance
-                          .collection("Location")
-                          .doc(FirebaseStaticVAriables.selected_location_id)
-                          .update({
-                        'currentLocation': GeoPoint(
-                            currentLocation.latitude!,
-                            currentLocation.longitude!)
-                      });
-
-                      ModelStatic.gps_share_flag = 1;
-                      ModelStatic.start_time = new DateTime.now();
-
-
-                            latt = currentLocation.latitude!.toString();
-                            lonn = currentLocation.longitude!.toString();
-                            ModelStatic.particularAppbarText = "location: $latt ,,$lonn ";
-
-
-
-
-                    });
+             });
           }
 
 
-          ModelStatic.gps_share_flag = 1;
-          Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => TarangaHomePage()));
-          //  Navigator.pop(context);
-          print("already pressed");
+          _finalAction();
         },
         child: Text("ShareLocation"));
   }
+
+  void _finalAction()
+  {
+
+    ModelStatic.gps_share_flag = 1;
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => TarangaHomePage()));
+    //  Navigator.pop(context);
+    print("Location Share Done");
+  }
+
+  void _updateAppBar(String lat,String lon)
+  {
+    print(lat);
+    print(lon);
+
+    ModelStatic.particularAppbarText = "location: $lat : $lon ";
+  }
+
+  int _timeTrack()
+  {
+    int time_flag = 1;
+    DateTime current_time = new DateTime.now();
+
+    if (current_time.year >
+        ModelStatic.start_time.year) {
+      time_flag = 0;
+    } else if (current_time.month >
+        ModelStatic.start_time.month) {
+      time_flag = 0;
+    } else if (current_time.day >
+        ModelStatic.start_time.day) {
+      time_flag = 0;
+    } else if (current_time.hour >
+        ModelStatic.start_time.hour + 4) {
+      time_flag = 0;
+    }
+
+         return time_flag;
+  }
+
+  void _timeFlagAction(int timeFlag)
+  {
+    if (timeFlag == 0) {
+      loc.Location.instance
+          .enableBackgroundMode(enable: false);
+      ModelStatic.locationSubscription
+          .cancel();
+
+      ModelStatic.gps_share_flag = 0;
+      print("app will restart");
+      timeRestartFlag = 1;
+    }
+  }
+
 }
--- lib/StaticPart/Firebase/FirebaseUpdate.dart
+++ lib/StaticPart/Firebase/FirebaseUpdate.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../BusStaticVariables.dart';
+import '../FirabaseStaticVariables.dart';
+
 class FirebaseUpdate{
 
   static Future<void> updateScheduleArray(String docId, String FieldName) async {
@@ -7,6 +10,19 @@ class FirebaseUpdate{
     List<String> locShare=   <String> ['2','0','1','1','1','1','1','1','1'];
     await FirebaseFirestore.instance.collection('schedule').doc(docId).update({
       FieldName: locShare ,
+    });
+
+  }
+
+  static Future<void> updateLocshareAndNotice() async {
+
+
+    await FirebaseFirestore.instance
+        .collection('schedule')
+        .doc(FirebaseStaticVAriables.selected_schedule_id)
+        .update({
+      "locShare": BusStaticVariables.locShare,
+      'notice': BusStaticVariables.Notice
     });
 
   }
